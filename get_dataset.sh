4ff2865a4b26b4e415fc9c407988f69cb5289430
chrisPixelCraft <rafaelhsieh0813@gmail.com>
2025-02-12 19:26:38 +0000

    concat dataset training + delete imbalanced dataset

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,6 +1,7 @@
 gdown 1wi3I8xGKpDQnITw3VnspS3iIfjIOmLiI -O datasets.zip
 gdown 1hS12pVi2nJmP8v2yJmiwllJLq8yPvLrJ -O Distributed-Real-Time-System.zip
-gdown 1tZ_merTuqkVOPGI1DSovOG3PhUckOMK- -O NTU-MTMC.zip
+# gdown 1tZ_merTuqkVOPGI1DSovOG3PhUckOMK- -O NTU-MTMC.zip
+curl -H "Authorization: Bearer ya29.a0AXeO80SlpsIDIHkjxM1qQVuKt2qT5CbMJ3gMegUm-L5-dNNqtURNSz4a4SnVnMS2cKc3gFzmxbwBurwylxaY4dcD2oKuWNnaQp2dWa8GVA9q4L62Ce_LKE0xyh1NU0QVFcX2I3ePshUxGVQy0gknQRrP7XU2GfTp58yRq89EaCgYKAeISARESFQHGX2MibGETL4BVzKXW7V3VIf9jhg0175" https://www.googleapis.com/drive/v3/files/1tZ_merTuqkVOPGI1DSovOG3PhUckOMK-?alt=media -o NTU-MTMC.zip
 gdown 1A5hetiQWPDMuTU1EfJjDTmABZp0jT6Vq -O other_sup.zip
 
 unzip datasets.zip
