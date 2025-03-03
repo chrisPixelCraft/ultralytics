eb927a599c9d918b66d2045faf457a1770fd5c4d
chrisPixelCraft <rafaelhsieh0813@gmail.com>
2025-03-03 20:18:58 +0000

    yolox11, resolution training stage 1, switch to 4090

--- get_ckpt.sh
+++ get_ckpt.sh
@@ -1,8 +1,8 @@
-gdown 1ML7K5c8Up-voOK3cT6eMPJQSmtEH7pFF -O train45.zip
-unzip train45.zip
+# gdown 1ML7K5c8Up-voOK3cT6eMPJQSmtEH7pFF -O train45.zip
+# unzip train45.zip
 
-gdown 1rGMl73yHNO8bdlJTIf8D-aR5o0t5bg4x -O train46.zip
-unzip train46.zip
+# gdown 1rGMl73yHNO8bdlJTIf8D-aR5o0t5bg4x -O train46.zip
+# unzip train46.zip
 
 gdown 1c5PZjOcoRRo3ZSUcZdfSChPfhWiaVYBU -O yolo11x.pt
 
--- get_dataset.sh
+++ get_dataset.sh
@@ -1,11 +1,13 @@
-gdown 1wi3I8xGKpDQnITw3VnspS3iIfjIOmLiI -O datasets.zip
-gdown 1hS12pVi2nJmP8v2yJmiwllJLq8yPvLrJ -O Distributed-Real-Time-System.zip
+# gdown 1wi3I8xGKpDQnITw3VnspS3iIfjIOmLiI -O datasets.zip
+gdown 1XTmT5dRUMjH4e48OuNn5ZJVTa0g3BOB4 -O concat.zip
+# gdown 1hS12pVi2nJmP8v2yJmiwllJLq8yPvLrJ -O Distributed-Real-Time-System.zip
 # gdown 1tZ_merTuqkVOPGI1DSovOG3PhUckOMK- -O NTU-MTMC.zip
 curl -H "Authorization: Bearer ya29.a0AeXRPp4CXNqTaBXLPSVDPDnqWb1q1o2txj_srNq_EgmDu3wQ0ZWZHxnqZkkhImmUnmjr3MykGDQ8Qrec1NaUmSBGGYTbM05wOlFm7tMiytGyP4A3V3wA5_MlKc8qBI74Q1jBgktp_2HoV9B94wHy_obp-eXtBwVk6f1KRtwsaCgYKAXISARESFQHGX2MiaNVGKy75Vu2rsxQJGV9xMA0175" https://www.googleapis.com/drive/v3/files/1tZ_merTuqkVOPGI1DSovOG3PhUckOMK-?alt=media -o NTU-MTMC.zip
-gdown 1A5hetiQWPDMuTU1EfJjDTmABZp0jT6Vq -O other_sup.zip
+# gdown 1A5hetiQWPDMuTU1EfJjDTmABZp0jT6Vq -O other_sup.zip
 
-unzip -n datasets.zip
-unzip -n Distributed-Real-Time-System.zip
+# unzip -n datasets.zip
+unzip concat.zip
+# unzip -n Distributed-Real-Time-System.zip
 unzip -n NTU-MTMC.zip
-unzip -n other_sup.zip
+# unzip -n other_sup.zip
 
